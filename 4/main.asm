77204fdaf64d1b7f98189a82b08d6be95e7a135d
0dminnimda <0dminnimda@gmail.com>
2024-04-12 00:59:51 +0300

    4\main.asm: add more test cases

--- 4/main.asm
+++ 4/main.asm
@@ -27,12 +27,17 @@ segment readable writable
     new_line_str.len = $-new_line_str
 
     ; flt dq -3.14
+    ; flt dq 0.0
+    ; flt dq -0.0
     ; flt dq -0.314
     ; flt dq -0.0314
     ; flt dq -0.00314
     ; flt dq 123345456457123345456457123345456457.5
-    flt dq 3e145
+    ; flt dq 3e145
     ; flt dq 4e-145
+    ; flt dq 9e-323
+    ; flt dq 2e-306
+    flt dq 1e+308
 
 
 ; display/i $pc
